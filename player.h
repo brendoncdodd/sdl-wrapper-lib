f77dbc305195ae87d2fc8797b27706cdf0142280
brendoncdodd <brendoncdodd@gmail.com>
2016-02-15 13:27:56 -0600

    Create player.h

---
+++ player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <SDL2\SDL.h>
+#include "visible_object.h"
+#include "game.h"
+
+class Player : public VisibleObject
+{
+	public:`
+		int draw();
+	private:
+		
+};
+
+#endif //PLAYER_H

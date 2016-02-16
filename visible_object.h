72689aa0b508bbdd65b9d828b7d7e7d345441c24
brendoncdodd <brendoncdodd@gmail.com>
2016-02-15 22:24:25 -0600

    GPLv3 boilerplate, added SDL header

--- visible_object.h
+++ visible_object.h
@@ -1,6 +1,23 @@
+/*
+*Copyright (C) {2016}  {Dodd, Brendon Charles}
+*
+*This program is free software: you can redistribute it and/or modify
+*it under the terms of the GNU General Public License as published by
+*the Free Software Foundation, version 3 of the License.
+*
+*This program is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License
+*along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef VISIBLE_OBJECT_H
 #define VISIBLE_OBJECT_H
 
+#include <SDL2/SDL.h>
 #include <list>
 
 class Game;	//Hint at Game class so we can have a pointer to it in the constructor

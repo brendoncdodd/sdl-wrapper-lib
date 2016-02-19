17fc3c0c870737a9a8708661426cd919bfda887c
brendoncdodd <brendoncdodd@gmail.com>
2016-02-19 14:09:17 -0600

    Update dodd_window.cpp

--- dodd_window.cpp
+++ dodd_window.cpp
@@ -23,7 +23,7 @@ DoddWindow::DoddWindow(int width, int height)
 	SDL_Init(SDL_INIT_VIDEO); 													//Initializing SDL2
 	
 	sdlWindow = SDL_CreateWindow("Dodd's Game", SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN); 				//Creating window
+		SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN); 		//Creating window
 	
 	sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_ACCELERATED); 	//Creating renderer
 }
